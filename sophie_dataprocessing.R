bde30e8cb554b4debbb9a84b0ba0c304c1aabd37
Sophie Yeh <sophie256@gmail.com>
2022-04-05 23:09:12 -0400

    new linear models

--- sophie_dataprocessing.R
+++ sophie_dataprocessing.R
@@ -3,6 +3,7 @@ library(car)
 library(GGally)
 
 money_formatter <- function(x) {
+  x <- gsub("â‚¬", "", x)
   x <- gsub("€", "", x)
   x <- case_when(
     grepl("K", x, fixed=TRUE) ~ as.numeric(gsub("K", "", x))*1e3,
@@ -19,9 +20,17 @@ weight_formatter <- function(x) {
   )
   return(x)
 }
+isMajor <- function(x, major) {
+  x <- case_when(
+    x %in% major ~ 1,
+    TRUE ~ 0
+  )
+  return(x)
+}
 
 data <- read.csv('data/external/FIFA22_official_data.csv')
 dim(data)
+glimpse(data)
 # analysis only involves goal-keepers
 df <- (filter(data, Best.Position == "GK")) #1546 rows
 # remove irrelevant string columns
@@ -46,12 +55,28 @@ df$Preferred.Foot[df$Preferred.Foot == "Left"] <- 0
 df <- rename(df, Preferred.Foot.Right = Preferred.Foot)
 df$Preferred.Foot.Right <- as.numeric(df$Preferred.Foot.Right)
 # convert contract valid date
-# df$Contract.Valid.Until <- (format(as.Date(df$Contract.Valid.Until, tryFormats=c("%B %d, %Y", "%Y")),"%Y"))
+df$Contract.Valid.Until <- as.numeric(format(as.Date(df$Contract.Valid.Until, tryFormats=c("%B %d, %Y", "%Y")),"%Y"))
+df$Contract.Years <- 2022 - df$Contract.Valid.Until
+
+# NATIONALITY
+by_nationality <- tally(df %>% group_by(Nationality))
+major_nationality <- by_nationality[by_nationality$n >= 20,]
+df$isMajorNationality <- isMajor(df$Nationality, major_nationality$Nationality)
+
+#CLUB
+byClub <- tally(df %>% group_by(Club))
+major_club <- byClub[byClub$n >= 5,]
+dim(major_club)
+df$isMajorClub <- isMajor(df$Club, major_club$Club)
+table(df$isMajorClub)
 
 df <- select(df,
              Value,
              Age, Overall, Potential,
+             isMajorNationality,
              Nationality,
+             Club,
+             isMajorClub,
              International.Reputation,
              Preferred.Foot.Right,
              Weak.Foot,
@@ -65,9 +90,9 @@ df <- select(df,
              Special,
              Height,
              Weight, 
+             Contract.Years,
              Release.Clause)
 # Release.Clause has 137 NAs
-# remove NA's so we can apply anova to compare models
 df <- na.omit(df)
 # remove 38 rows of value == 0, which are outliers
 df <- df[df$Value != 0,]
